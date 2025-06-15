1d3054f0f625a66828c6937665d6acb8ec3749df
89Explorer <leral3@naver.com>
2025-06-16 00:44:19 +0900

    Initial Commit

---
+++ PhotoDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoDiary
+//
+//  Created by 권정근 on 6/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
